d4b372b14ac96cadfa02af85cff04da81d09e44d
ksenuch <pligina.k@inbox.ru>
2025-05-20 12:50:54 +0600

    Выполнен рефакторинг FindPhone с учетом с учетом новой системы тегов

--- lab_5_interpreter/src/main/java/phones/FindPhone.java
+++ lab_5_interpreter/src/main/java/phones/FindPhone.java
@@ -1,44 +1,47 @@
 package phones;
+
 import java.awt.Color;
 import java.util.LinkedList;
 import java.util.List;
 
 public class FindPhone {
-    private List<Phone> data=new PhoneDB().getPhoneData();
-    
+    private List<Phone> data = new PhoneDB().getPhoneData(); // Источник данных
+    // Основной метод поиска с использованием интерпретатора
+    public List<Phone> find(Tag tag) {
+        List<Phone> result = new LinkedList<>();
+        for (Phone ph : data) {
+            if (tag.find(ph)) {// Интерпретация условия для каждого телефона
+                result.add(ph);
+            }
+        }
+        return result;
+    }
+
+    // Старые методы оставлены для обратной совместимости, но теперь они используют новый подход
+// Примеры готовых запросов (используют интерпретатор)
     public List<Phone> byColor(Color col) {
-        List<Phone> find=new LinkedList<>();
-        for(Phone ph : data)
-            if (ph.getColor()==col)   find.add(ph);
-        return find;
+        return find(new ColorTag(col));
     }
-    
+
     public List<Phone> byModel(String mod) {
-        List<Phone> find=new LinkedList<>();
-        for(Phone ph : data)
-            if (ph.getModel().equalsIgnoreCase(mod))   find.add(ph);
-        return find;
+        return find(new ModelTag(mod));
     }
-    
+
     public List<Phone> byMSize(int memSize) {
-        List<Phone> find=new LinkedList<>();
-        for(Phone ph : data)
-            if (ph.getMemorySize()==memSize)   find.add(ph);
-        return find;
+        return find(new MemorySizeTag(memSize));
     }
-    
+
     public List<Phone> byModelAndPriceLow(String mod, double price) {
-        List<Phone> find=new LinkedList<>();
-        for(Phone ph : data)
-            if (ph.getModel().equalsIgnoreCase(mod) &&
-                                ph.getPrice()<price)   find.add(ph);
-        return find;
+        return find(new AndTag(
+                new ModelTag(mod),
+                new PriceLowerTag(price)
+        ));
     }
-    
+
     public List<Phone> byMSizeAndNotColor(int memSize, Color col) {
-        List<Phone> find=new LinkedList<>();
-        for(Phone ph : data)
-            if (ph.getMemorySize()==memSize && !(ph.getColor()==col))   find.add(ph);
-        return find;
-    }    
+        return find(new AndTag(
+                new MemorySizeTag(memSize),
+                new NotTag(new ColorTag(col))
+        ));
+    }
 }
